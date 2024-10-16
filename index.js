97597f14998d0f3a2d8317071dbd93ba179b7b87
kennedysenyo <kennedysenyo@gmail.com>
2024-10-16 21:23:57 +0000

    Add keyboard support to play drum sounds

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 
 
+const audio = new Audio();
 
 const drumButtons = document.querySelectorAll(".drum");
 for(i = 0; i < drumButtons.length; i++) {  
   drumButtons[i].addEventListener("click", function () {
-
-    const audio = new Audio();
-    switch(this.classList[0]) {
-      case "w":
-        audio.src = "sounds/crash.mp3";
-        audio.play();
-        break;
-      case "a":
-        audio.src = "sounds/kick-bass.mp3";
-        audio.play();
-        break;
-      case "s":
-        audio.src = "sounds/snare.mp3";
-        audio.play();
-        break;
-      case "d":
-        audio.src = "sounds/tom-1.mp3";
-        audio.play();
-        break;
-      case "j":
-        audio.src = "sounds/tom-2.mp3";
-        audio.play();
-        break;
-      case "k":
-        audio.src = "sounds/tom-3.mp3";
-        audio.play();
-        break;
-        case "l":
-          audio.src = "sounds/tom-4.mp3";
-        audio.play();
-        break;
-
-      }
+    playSound(this.classList[0])   
   }
 );
+}
+
+document.addEventListener("keypress", function (event) {
+  playSound(event.key)
+});
+
+function playSound(key) {
+  switch(key) {
+  case "w":
+    audio.src = "sounds/crash.mp3";
+    audio.play();
+    break;
+  case "a":
+    audio.src = "sounds/kick-bass.mp3";
+    audio.play();
+    break;
+  case "s":
+    audio.src = "sounds/snare.mp3";
+    audio.play();
+    break;
+  case "d":
+    audio.src = "sounds/tom-1.mp3";
+    audio.play();
+    break;
+  case "j":
+    audio.src = "sounds/tom-2.mp3";
+    audio.play();
+    break;
+  case "k":
+    audio.src = "sounds/tom-3.mp3";
+    audio.play();
+    break;
+  case "l":
+    audio.src = "sounds/tom-4.mp3";
+    audio.play();
+    break;
+  }
 }
